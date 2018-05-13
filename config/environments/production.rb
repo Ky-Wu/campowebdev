aad0b65535064d66d1ed8ec56c4c14587c007c09
Kyle <kylelinwu@gmail.com>
2018-05-13 16:14:56 -0700

    Set up bootstrapped devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Set up default url option for Devise Mailer
+  config.action_mailer.default_url_options = { host: 'campowebdev.herokuapp.com' }
 end
