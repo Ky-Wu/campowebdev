652799ef7af1c680fab54cd585988c00ef980562
Kyle <kylelinwu@gmail.com>
2018-05-13 17:02:47 -0700

    Add proto-navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'login' }
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
 end
