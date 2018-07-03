7a3f3ec2709d839cf8ea7e99dd81c0f0e9f78a66
Kyle <kylelinwu@gmail.com>
2018-07-02 22:30:16 -0700

    Fix typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  validates :email, presence :true, if: :domain_check
+  validates :email, presence: true, if: :domain_check
   
   APPROVED_DOMAINS = ["auhsdschools.org"]
   def domain_check
