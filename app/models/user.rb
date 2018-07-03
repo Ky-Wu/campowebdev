3f8cf15932e6bd9fad3a4d7018c02d95941f3452
Kyle <kylelinwu@gmail.com>
2018-07-02 22:15:02 -0700

    Add domain restriction on users when registering

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  validates :email, presence :true, if: :domain_check
+  
+  APPROVED_DOMAINS = ["auhsdschools.org"]
+  def domain_check
+    unless APPROVED_DOMAINS.any? { |word| email.end_with?(word) }
+      errors.add(:email, "is not an auhsdschools.org email (Campo students only" )
+    end
+  end
 end
